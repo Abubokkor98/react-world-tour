cea8a8403c888a58902543dc1405a267e4068dcf
Abubokkor98 <mail.abubokkor@gmail.com>
2024-10-19 21:51:57 +0600

    Toggle state based on click and conditional rending

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Country from '../Country/Country';
+import './Countries.css'
 
 export default function Countries() {
     const [countries, setCountries] = useState([]);
@@ -13,11 +14,13 @@ export default function Countries() {
   return (
     <div>
         <h3>Countries{countries.length}</h3>
+        <div className='country-container'>
         {
             countries.map(country=> <Country 
                 country={country}
                 key={country.cca3}></Country>)
         }
+        </div>
     </div>
   )
 }
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Country.css'
 
 export default function Country({country}) {
     console.log(country);
-    const {name, flags, population, area} = country;
+    const {name, flags, population, area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
   return (
     <div className='country'>
         <h3>Country: {name.common}</h3>
         <img src={flags.png} alt="" />
         <p>Population: {population}</p>
         <p>Area: {area}</p>
+        <p><small>Code: {cca3}</small></p>
+        <button onClick={handleVisited}>{visited? "Visited" : 'Visit'}</button>
+        {visited ? 'I Have Visited This Country.' : 'I want to Visit'}
 
     </div>
   )
